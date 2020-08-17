2ab4266e5b07702b2239642996d8b972a96bf558
saravana-balaji <69804644+saravana-balaji@users.noreply.github.com>
2020-08-17 20:31:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyAnFjMuj5df055nmGtXCtN6qNdy5HtCqRs",
+  authDomain: "web-design-455c3.firebaseapp.com",
+  databaseURL: "https://web-design-455c3.firebaseio.com",
+  projectId: "web-design-455c3",
+  storageBucket: "web-design-455c3.appspot.com",
+  messagingSenderId: "171134795564",
+  appId: "1:171134795564:web:68ca8fab215e39fbbc3689",
+  measurementId: "G-SK1FMBQ44C"
 };
 
      
